896c044189ea0ea5518252b8659c667e6aa5134a
rudieros <rudierosdfr@gmail.com>
2020-09-03 11:41:16 -0300

    Update pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import { theme } from "../src/config/theme";
 
 /**
  * With this file, we can customize the React entry point for every page of our website
- * Here we can add components that appear on every site, or even hold state that all pages will use
+ * Here we can add components that appear on every screen, or even hold state that all pages will use
  * read more: https://nextjs.org/docs/advanced-features/custom-app
  */
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
