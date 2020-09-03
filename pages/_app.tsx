aa0e8d00a7e32f46ea0c00d340da88ba7a9d8bd1
rudieros <rudierosdfr@gmail.com>
2020-09-03 17:45:08 -0300

    Update pages/_app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,7 +25,7 @@ if (!isServer) {
 
 /**
  * With this file, we can customize the React entry point for every page of our website
- * Here we can add components that appear on every site, or even hold state that all pages will use
+ * Here we can add components that appear on every screen, or even hold state that all pages will use
  * read more: https://nextjs.org/docs/advanced-features/custom-app
  */
 class CustomApp extends App<
