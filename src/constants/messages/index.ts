4bb9ba369e5635e25d897af7ea865892e520c9af
Thiago Fernandes <thiago.fernandes@provi.com.br>
2020-09-09 15:12:12 -0300

    remove duplicate logic

--- src/constants/messages/index.ts
+++ src/constants/messages/index.ts
@@ -2,18 +2,18 @@ import { messages_en } from "../strings/translations/en";
 import { messages_pt } from "../strings/translations/ptbr";
 import detectBrowserLanguage from "detect-browser-language";
 
+// TODO: pegar a função checkIfServer, que ainda não ta na master
+
 let appLanguage = messages_en;
 
+let browserLanguage = "en";
 if (typeof window !== "undefined") {
-  let browserLanguage = "en";
-  if (typeof window !== "undefined") {
-    browserLanguage = detectBrowserLanguage();
-  }
-  const languageWithoutRegion: string = browserLanguage.split(/[-_]/)[0];
+  browserLanguage = detectBrowserLanguage();
+}
+const languageWithoutRegion: string = browserLanguage.split(/[-_]/)[0];
 
-  if (languageWithoutRegion === "pt") {
-    appLanguage = messages_pt;
-  }
+if (languageWithoutRegion === "pt") {
+  appLanguage = messages_pt;
 }
 
 export const messages = appLanguage;
