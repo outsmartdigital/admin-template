10c5a9550a8af64efce55a4ac3d5ae4a2d08e72c
Thiago Fernandes <thiago.fernandes@provi.com.br>
2020-09-10 11:36:22 -0300

    get preferred language from server

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,9 @@ import { theme } from "../src/config/theme";
 
 import { IntlProvider } from "react-intl";
 import { messages } from "../src/constants/messages";
-import { useIntl } from "../src/constants/intl/intlHooks";
+// import { useIntl } from "../src/constants/intl/intlHooks";
+
+import App, { AppContext } from "next/app";
 
 /**
  * With this file, we can customize the React entry point for every page of our website
@@ -14,17 +16,78 @@ import { useIntl } from "../src/constants/intl/intlHooks";
  * read more: https://nextjs.org/docs/advanced-features/custom-app
  */
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => {
-  const [translatedMessage, language] = useIntl();
+// const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+//   const [translatedMessage, language] = useIntl();
 
-  return (
-    <IntlProvider locale={language} defaultLocale={"en"} messages={messages}>
-      <ThemeProvider theme={theme}>
-        <AppHeader />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </IntlProvider>
-  );
+//   return (
+//     <IntlProvider locale={language} defaultLocale={"en"} messages={messages}>
+//       <ThemeProvider theme={theme}>
+//         <AppHeader />
+//         <Component {...pageProps} />
+//       </ThemeProvider>
+//     </IntlProvider>
+//   );
+// };
+
+// const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+//   const [translatedMessage, language] = useIntl();
+
+//   return (
+//     <IntlProvider locale={language} defaultLocale={"en"} messages={messages}>
+//       <ThemeProvider theme={theme}>
+//         <AppHeader />
+//         <Component {...pageProps} />
+//       </ThemeProvider>
+//     </IntlProvider>
+//   );
+// };
+
+// export default App;
+
+export const configLanguage = ctx => {
+  const { req } = ctx;
+  const language = req
+    ? req.header["accept-language"]
+    : window.navigator.language;
+
+  return language;
 };
 
-export default App;
+class CustomApp extends App<
+  AppProps & { GlobalStateProvider: React.ComponentType }
+> {
+  static async getInitialProps({ ctx, Component }: AppContext) {
+    let pageProps = {};
+
+    const { req } = ctx;
+    const language = req
+      ? req.headers["accept-language"]
+      : window.navigator.language;
+
+    console.log("Language é:", language);
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return {
+      pageProps,
+      language
+    };
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <IntlProvider locale={"en"} defaultLocale={"pt"} messages={messages}>
+        <ThemeProvider theme={theme}>
+          <AppHeader />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </IntlProvider>
+    );
+  }
+}
+
+export default CustomApp;
